3009afc13be899a65f6999ccedb7a164cf8ebf86
style <style@cs.technion.ac.il>
2020-07-06 21:05:34 -0400

    removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,10 +53,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
   }
 
   logout() {
