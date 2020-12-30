2a9fd6bb701e6ded09d1afe2f01afb614ff76e75
style <style@cs.technion.ac.il>
2020-12-30 15:43:30 -0500

    fix sum of cancelled interviews

--- src/app/iio/list/groupby.pipe.ts
+++ src/app/iio/list/groupby.pipe.ts
@@ -1,20 +1,21 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {DateTime} from 'luxon';
+import {InterviewInterface, InterviewStatus} from "../iio.service";
 
 @Pipe({name: 'groupByMonth'})
 export class GroupByMonthPipe implements PipeTransform {
 
-  transform(array) {
-    return array.reduce((acc, curr) => {
+  transform(array: Array<InterviewInterface>) {
+    return array.reduce((acc, curr: InterviewInterface) => {
       const plannedDate = DateTime.fromJSDate(new Date(curr.plannedDate)).toFormat('yyyy-MM');
       let item = acc.find(item => item.month === plannedDate);
       if (item) {
         item.events.push(curr);
-        item.total += +curr.value;
+        item.total += (curr.status === InterviewStatus.Cancelled) ? 0 : +curr.value;
       } else {
         acc.push({
           'month': plannedDate,
-          'total': +curr.value,
+          'total': (curr.status === InterviewStatus.Cancelled) ? 0 : +curr.value,
           'events': [curr]
         });
       }
