9551697a4f377e2ed6e0577f980bdb175b687bd8
style <style@cs.technion.ac.il>
2021-02-14 23:26:38 -0500

    groupby collects info by status

--- src/app/iio/list/groupby.pipe.ts
+++ src/app/iio/list/groupby.pipe.ts
@@ -9,16 +9,22 @@ export class GroupByMonthPipe implements PipeTransform {
     return array.reduce((acc, curr: InterviewInterface) => {
       const plannedDate = DateTime.fromJSDate(new Date(curr.plannedDate)).toFormat('yyyy-MM');
       let item = acc.find(item => item.month === plannedDate);
-      if (item) {
-        item.events.push(curr);
-        item.total += (curr.status === InterviewStatus.Cancelled) ? 0 : +curr.value;
-      } else {
-        acc.push({
+      if (!item) {
+        item = {
           'month': plannedDate,
-          'total': (curr.status === InterviewStatus.Cancelled) ? 0 : +curr.value,
-          'events': [curr]
-        });
+          'total': 0,
+          'status': {},
+          'events': []
+        };
+        acc.push(item);
       }
+      item.events.push(curr);
+      item.total += (curr.status === InterviewStatus.Cancelled) ? 0 : +curr.value;
+      if (!item.status[curr.status]) {
+        item.status[curr.status] = [0, 0];
+      }
+      item.status[curr.status][0] += 1;
+      item.status[curr.status][1] += +curr.value;
       return acc;
     }, []);
   }
