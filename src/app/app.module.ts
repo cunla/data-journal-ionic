1255f826d459bf1781b0904bba64a6c12eea437d
DanielM <daniel@moransoftware.ca>
2023-12-20 18:11:04 +0000

    chore(deps):update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import {AppRoutingModule} from './app-routing.module';
 import {AuthModule} from './auth/auth.module';
 import {BloodresultsModule} from './bloodresults/bloodresults.module';
 import {GoogleMapsModule} from "@angular/google-maps";
+import {environment} from '../environments/environment';
+import {AngularFireModule} from '@angular/fire/compat';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+
 
 @NgModule({
   declarations: [AppComponent,],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
     BloodresultsModule,
     BrowserModule,
     IonicModule.forRoot(),
