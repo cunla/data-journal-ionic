cfc28fbed47c32c693695720133a3a36233cdcc1
Daniel M <daniel@moransoftware.ca>
2026-06-19 09:40:13 +0000

    update deprecated packages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -9,5 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowser().bootstrapModule(AppModule)
   .catch(err => console.log(err));
